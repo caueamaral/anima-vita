c2b64c694ba813119fc6b32dc4a54177629a96e9
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-17 00:37:59 -0300

    Footer using styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,31 @@
+import styled from 'styled-components'
+
+const Container = styled.footer`
+  background: var(--white);
+  font-family: var(--titleFont);
+  font-size: 18px;
+  letter-spacing: 1px;
+  padding: 30px 20px;
+  text-align: center;
+  width: 100%;
+
+  .green {
+    color: #42cca2;
+  }
+
+  .blue {
+    color: #185b9d;
+  }
+`
+
 function Footer() {
   return (
-    <footer className="footer">
-      <p>
-        <strong>
-          <em className="footer-emphasis">
-            <span className="footer-green">Anima</span><span className="footer-blue">Vita</span>
-          </em> 2021 - All rights reserved.
-        </strong>
-      </p>
-    </footer>
+    <Container>
+      <strong>
+        <span className="green">Anima</span>
+        <span className="blue">Vita</span> 2021 - All rights reserved.
+      </strong>
+    </Container>
   )
 }
 

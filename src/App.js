ffa2f7416c79e5c49a56594fa7bd9e328f2441d6
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-17 01:58:08 -0300

    Generic CSS

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import './App.css'
 import Header from './components/Header/index.js'
 import Main   from './components/Main/index.js'
 import Footer from './components/Footer/index.js'
-import styled from 'styled-components'
-
-const AppWrapper = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  min-height: 100vh;
-`
 
 function App() {
   return (
-    <AppWrapper>
+    <div className="app">
       <Header />
       <Main />
       <Footer />
-    </AppWrapper>
+    </div>
   );
 }
 

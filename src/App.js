28bf7bd506f4b77e08d5e37bf2ad888c2e01e179
Caue Amaral de Souza <caue.amaral@hotmail.com>
2021-05-17 01:19:41 -0300

    Container/App with styled-components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css'
 import Header from './components/Header.js'
 import Main   from './components/Main.js'
 import Footer from './components/Footer.js'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+`
 
 function App() {
   return (
-    <div className="app">
+    <Container>
       <Header />
       <Main />
       <Footer />
-    </div>
+    </Container>
   );
 }
 
